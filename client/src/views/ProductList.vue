d3100eca8b95315c0023cc4ce48ac17542e5fa8d
DizzyDwarf10 <jjkadet@icloud.com>
2026-03-09 15:15:47 -0400

    products look changed

--- client/src/views/ProductList.vue
+++ client/src/views/ProductList.vue
@@ -21,20 +21,24 @@ onMounted(() => {
     </div>
 
     <div v-else class="product-list">
-        <div v-for="product in productStore.products" :key="product.id" class="card mb-4">
-            <div class="card-content">
-                <div class="media">
-                    <div class="media-left">
-                        <figure class="image is-64x64">
-                            <img :src="product.thumbnail" :alt="product.title">
-                        </figure>
-                    </div>
-                    <div class="media-content">
-                        <p class="title is-4">{{ product.title }}</p>
-                        <p class="subtitle is-6">{{ product.brand }} - {{ product.category }}</p>
-                        <p class="content">{{ product.description }}</p>
-                        <p class="has-text-weight-bold">${{ product.price }}</p>
-                        <p class="has-text-grey">Rating: {{ product.rating }}/5 ({{ product.stock }} in stock)</p>
+        <div class="columns is-multiline">
+            <div v-for="product in productStore.products" :key="product.id" class="column is-one-quarter-desktop is-half-tablet is-full-mobile">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-left">
+                                <figure class="image is-64x64">
+                                    <img :src="product.thumbnail" :alt="product.title">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-5">{{ product.title }}</p>
+                                <p class="subtitle is-7">{{ product.brand }} - {{ product.category }}</p>
+                                <p class="content is-small">{{ product.description }}</p>
+                                <p class="has-text-weight-bold">${{ product.price }}</p>
+                                <p class="has-text-grey is-small">Rating: {{ product.rating }}/5 ({{ product.stock }} in stock)</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
