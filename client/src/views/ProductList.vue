537dd165a22c7c8319bfad0ca1f4380b738620d8
DizzyDwarf10 <jjkadet@icloud.com>
2026-03-09 14:44:22 -0400

    run build fix

---
+++ client/src/views/ProductList.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1 class ="title is-1">Product List</h1>
+    <div class="product-list">
+
+    </div>
+</template>
+
+<style scoped></style>
