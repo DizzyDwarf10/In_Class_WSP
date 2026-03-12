3ff5219f4b09d3391302a88636f3cea801895539
DizzyDwarf10 <jjkadet@icloud.com>
2026-03-12 16:38:17 -0400

    no data

--- client/src/views/ProductList.vue
+++ client/src/views/ProductList.vue
@@ -1,37 +1,62 @@
 <script setup lang="ts">
-import { useProductStore } from '@/stores/product';
-import type { Product } from '@/types';
-import { ref, onMounted } from 'vue';
+import type { Product } from "@/types"
+import { useProductStore } from "../stores/product"
+import { useCartStore } from "../stores/cart"
+import { onMounted } from "vue"
 
-const products = ref<Product[]>([]);
-const productStore = useProductStore();
+const productStore = useProductStore()
+const cartStore = useCartStore()
 
-onMounted(async () => {
-    await productStore.fetchProducts();
-    products.value = productStore.products;
-});
+onMounted(() => {
+    productStore.fetchProducts()
+})
+
+const addToCart = (product: Product) => {
+    cartStore.addItem(product)
+    console.log("Added to cart:", product)
+}
 </script>
-
 
 <template>
     <h1 class="title is-1">Product List</h1>
-    <div class="grid is-col-min-10">
-        <div v-for="product in products" :key="product.id" class="box">
-            <img :src="product.thumbnail" alt="Product Image" class="image is-4by3">
-            <button class="button is-primary is-small add-button">
-                Add to Cart
-            </button>
-            <b>{{ product.title }}</b>
-            {{ product.description }}
-            <div class="price">{{ product.price }}</div>
+
+    <div v-if="productStore.loading" class="notification is-info">
+        Loading products...
+    </div>
+
+    <div v-else-if="productStore.error" class="notification is-danger">
+        Error: {{ productStore.error }}
+    </div>
+
+    <div v-else class="product-list">
+        <div class="columns is-multiline">
+            <div v-for="product in productStore.products" :key="product.id" class="column is-one-quarter-desktop is-half-tablet is-full-mobile">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-left">
+                                <figure class="image is-64x64">
+                                    <img :src="product.thumbnail" :alt="product.title">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-5">{{ product.title }}</p>
+                                <p class="subtitle is-7">{{ product.brand }} - {{ product.category }}</p>
+                                <p class="content is-small">{{ product.description }}</p>
+                                <p class="has-text-weight-bold">${{ product.price }}</p>
+                                <p class="has-text-grey is-small">Rating: {{ product.rating }}/5 ({{ product.stock }} in stock)</p>
+                            </div>
+                        </div>
+                    </div>
+                    <footer class="card-footer">
+                        <button class="card-footer-item button is-primary" @click="addToCart(product)">
+                            Add to Cart
+                        </button>
+                    </footer>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
-<style scoped>
-.add-button {
-    float: right;
-    margin-top: .5em;
-    ;
-}
-</style>
+<style scoped></style>
